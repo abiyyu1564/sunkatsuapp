5431893c09067c6c60ff8d31b7f1fa5613447709
Raygama <daffaraygama55@gmail.com>
2024-10-26 23:09:18 +0700

    Update staffHistoryCard.jsx

--- sunkatsu_fe/src/components/Fragment/staffHistoryCard.jsx
+++ sunkatsu_fe/src/components/Fragment/staffHistoryCard.jsx
@@ -8,7 +8,7 @@ export default function StaffHistoryCard() {
             <img src={Profile} alt="profile" className="w-[150px] h-[176px] rounded-full" />
         
             <div className="relative w-full px-5">
-                <div className="grid grid-cols-2 gap-3">
+                <div className="grid grid-cols-3 gap-3">
                     <div>
                         <h1 className="text-[24px] font-bold text-[#FF0000]">Raygama</h1>
                         <p className="text-[16px]">Chicken Katsu Curry</p>
@@ -20,15 +20,18 @@ export default function StaffHistoryCard() {
                         <p className="text-[16px]">Quantity: 1 x 23.000</p>
                         <p className="text-[16px]">Quantity: 1 x 15.000</p>
                     </div>
+                    <div className="flex flex-col items-end">
+                        <div className="items-center">
+                        <p className="text-[16px] text-zinc-950 font-semibold text-center">Not Paid</p>
+                        <button className="bg-red-600 text-white text-[16px] w-[174px] h-[50px] rounded-full mt-2">
+                            Terima
+                        </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         
-            <div className="flex flex-col items-center">
-                <p className="text-[16px] text-zinc-950 font-semibold">Not Paid</p>
-                <button className="bg-red-600 text-white text-[16px] w-[174px] h-[50px] rounded-full mt-2">
-                    Terima
-                </button>
-            </div>
+            
 
             <div className="w-0,5 h-36 my-2 flex flex-col justify-center items-center border-2"></div>
         
