70abc5e0392c6b6f99bebd4c62bbcc2f6f233d52
rifqykhuzaini <rifqykhuzaini@gmail.com>
2024-10-30 14:30:08 +0700

    clearing frontend unused folders

--- backend/src/main/java/com/sunkatsu/backend/repositories/MenuRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sunkatsu.backend.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import com.sunkatsu.backend.models.*;
-
-@Repository
-public interface MenuRepository extends MongoRepository<Menu, Integer> {
-}
