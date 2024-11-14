1eed9a085968531beda8d343114de0a1d1ba128d
ftr867 <fathuraldesrand03@gmail.com>
2024-11-14 07:28:14 +0700

    add Backend for Favorite (model,repository)
    Model dan Repository, untuk service dan controller masih bingung

---
+++ backend/src/main/java/com/sunkatsu/backend/repositories/FavoriteRepository.java
@@ -0,0 +1,9 @@
+package com.sunkatsu.backend.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import com.sunkatsu.backend.models.*;
+
+@Repository
+public interface FavoriteRepository extends MongoRepository<Favorite, Integer> {
+}
