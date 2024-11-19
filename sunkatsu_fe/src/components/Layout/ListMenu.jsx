04646655d681503ade4d23edb45c251109ab0fbe
abiyyu1564 <abiyyuabdurrafi14@gmail.com>
2024-11-19 16:03:19 +0700

    index on main: 1cdf8dd Merge branch 'main' of https://github.com/abiyyu1564/sunkatsuapp

---
+++ sunkatsu_fe/src/components/Layout/ListMenu.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NewNavbar from "../Fragment/newLandingNavbar";
+import NewMenuCard from "../Fragment/newMenuCard";
+import NewLandingFooter from "../Fragment/newLandingFooter";
+
+const ListMenu = () => {
+  return (
+    <>
+      <NewNavbar />
+      <NewMenuCard />
+      <NewLandingFooter />
+    </>
+  );
+};
+
+export default ListMenu;
