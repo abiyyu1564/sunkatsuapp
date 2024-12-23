83677f4436886ed1d4717526f18e2234052ff315
Raygama <daffaraygama55@gmail.com>
2024-12-24 01:34:42 +0700

    Tuning Chatbot

--- backend/src/main/java/com/sunkatsu/backend/controllers/ChatbotController.java
+++ backend/src/main/java/com/sunkatsu/backend/controllers/ChatbotController.java
@@ -24,7 +24,7 @@ public class ChatbotController {
                     menggunakan informasi yang relevan dari dokumen FAQ berikut:
                     [FAQ
                     1. Q: Bagaimana cara memesan makanan?
-                    A: Pilih menu, lalu finish cart, nanti order akan terlihat di laman order.
+                    A: Pilih menu, lalu add to cart. Jika sudah selesai finish cart, nanti order akan terlihat di laman order.
 
                     2. Q: Bagaimana cara saya membayar secara online?
                     A: Tidak ada pembayaran online, silahkan bayar langsung ke tenant sunkatsu.
@@ -67,8 +67,20 @@ public class ChatbotController {
 
                     15. Q: Bagaimana cara saya chat ke pihak staff?
                     A: Di bagian main menu, pilih tanda gelembung bicara di sudut kanan atas].
+
+                    Selain itu terdapat informasi penting terkait aplikasi:
+                    1. Menu berada pada laman 'Menu'
+                    2. Pesanan yang anda sudah pesan berada pada laman 'My Order', terdapat deadline payment sekitar 1 setengah jam setelah pemesanan
+                    3. Jika anda sudah memilih menu, anda dapat menambahkan menu tersebut ke shopping cart. Shopping cart bisa dilihat di laman 'Cart' ditandai dengan icon tas belanja
+                    4. Chat kepada Staff, Owner, dan Customer lain bisa dilakukan pada lama 'Chat' dengan menekan tombol di kanan atas dengan icon gelembung bicara
+                    5. Anda bisa melihat riwayat pesanan anda pada laman 'My Order'
+                    6. Anda perlu login untuk mengakses aplikasi ini dengan username dan password
+                    7. Anda bisa logout dengan klik tombol logout di navbar
+                    8. Order Anda yang sudah melewati deadline akan otomatis terhapus jika tidak diambil. Mohon segera melakukan pembayaran baik melalui Staff di Chat atau di tenant langsung.
+                    9. Anda bisa melakukan filter Menu berdasarkan kategori yang sudah ada di laman 'Menu'
+                    10. Untuk sekarang belum ada fitur ganti password
                     
-                    Jika Anda tidak tahu jawabannya, katakan 'Maaf, saya tidak dapat menjawab pertanyaan tersebut.'
+                    Jika Anda tidak tahu jawabannya, katakan 'Maaf, saya tidak dapat menjawab pertanyaan tersebut. Hubungilah Staff melalui laman Chat untuk pertanyaan lebih lanjut.' jangan pernah sebut bahwa anda mengambil/mengakses/mengetahui Dokumen FAQ. Anda hanya perlu mengimitasi sebuah Customer Service layanan aplikasi
                     """)
                 .user(message)
                 .call()
@@ -83,7 +95,7 @@ public class ChatbotController {
                     menggunakan informasi yang relevan dari dokumen FAQ berikut:
                     [FAQ
                     1. Q: Bagaimana cara memesan makanan?
-                    A: Pilih menu, lalu finish cart, nanti order akan terlihat di laman order.
+                    A: Pilih menu, lalu add to cart. Jika sudah selesai finish cart, nanti order akan terlihat di laman order.
 
                     2. Q: Bagaimana cara saya membayar secara online?
                     A: Tidak ada pembayaran online, silahkan bayar langsung ke tenant sunkatsu.
@@ -126,8 +138,22 @@ public class ChatbotController {
 
                     15. Q: Bagaimana cara saya chat ke pihak staff?
                     A: Di bagian main menu, pilih tanda gelembung bicara di sudut kanan atas].
+
+                    Selain itu terdapat informasi penting terkait aplikasi Sunkatsu:
+                    1. Menu berada pada laman 'Menu'
+                    2. Pesanan yang anda sudah pesan berada pada laman 'My Order', terdapat deadline payment sekitar 1 setengah jam setelah pemesanan
+                    3. Jika anda sudah memilih menu, anda dapat menambahkan menu tersebut ke shopping cart. Shopping cart bisa dilihat di laman 'Cart' ditandai dengan icon tas belanja
+                    4. Chat kepada Staff, Owner, dan Customer lain bisa dilakukan pada lama 'Chat' dengan menekan tombol di kanan atas dengan icon gelembung bicara
+                    5. Anda bisa melihat riwayat pesanan anda pada laman 'My Order'
+                    6. Anda perlu login untuk mengakses aplikasi ini dengan username dan password
+                    7. Anda bisa logout dengan klik tombol logout di navbar
+                    8. Order Anda yang sudah melewati deadline akan otomatis terhapus jika tidak diambil. Mohon segera melakukan pembayaran baik melalui Staff di Chat atau di tenant langsung.
+                    9. Anda bisa melakukan filter Menu berdasarkan kategori yang sudah ada di laman 'Menu'
+                    10. Menu terbagi menjadi 3 kategori, yaitu 'food', 'drink', atau 'dessert'
+                    11. Order terbagi menjadi 3 status yaitu 'Not Paid', ketika Order belum dibayar namun sudah masuk. 'Accepted', ketika order sudah dibayar dan diterima staff. Dan 'Finished', ketika Order Customer sudah selesai.
                     
-                    Jika Anda tidak tahu jawabannya, katakan 'Maaf, saya tidak dapat menjawab pertanyaan tersebut.'
+                    Jika Anda tidak tahu jawabannya, katakan 'Maaf, saya tidak dapat menjawab pertanyaan tersebut. Hubungilah Staff melalui laman Chat untuk pertanyaan lebih lanjut.' jangan pernah sebut bahwa anda mengambil/mengakses/mengetahui Dokumen FAQ. Anda hanya perlu mengimitasi sebuah Customer Service layanan aplikasi.
+                    Jawablahh dengan nada sangat sopan dan formal untuk meningkatkan kepercayaan pelanggan. Anda bisa menambahkan jawaban sesuai logika Anda terkait aplikasi ini.
                     """)
                 .user(message)
                 .stream()
