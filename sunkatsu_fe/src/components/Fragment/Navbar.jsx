6ad57fe1ebc28b14b0a2334376c9be46f78fc4d4
melindatr <melindatriandari01@gmail.com>
2025-06-04 15:35:55 +0700

    update

--- sunkatsu_fe/src/components/Fragment/Navbar.jsx
+++ sunkatsu_fe/src/components/Fragment/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = () => {
 
   return (
     <nav className="fixed top-0 w-full h-16 z-[99] bg-white border-b border-gray-200">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-5 px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
           <div href="/" className="flex-shrink-0 ">
